7de59d67088d6b58d93207f341944398f32811e1
SupradeepDanturti <suppualn@gmail.com>
2024-04-15 18:25:09 -0400

    interface and results

--- results/ECAPA/Augmented/1986/train_ecapa_tdnn.py
+++ results/ECAPA/Augmented/1986/train_ecapa_tdnn.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91cb42aba0fd032324d89c6ac02c3b13d7cd54f8defc27118aaeb14fe804ba30
-size 7253
+oid sha256:b6270f91b711f8af2896dc3558f0da14b4a11b2c847941fd20a9f75307544c83
+size 5719
