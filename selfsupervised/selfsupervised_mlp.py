9fa98ca9e2fe0d096403644b3a05ac535c5d7502
SupradeepDanturti <suppualn@gmail.com>
2024-04-15 00:18:14 -0400

    docstring to selfsupervised code

--- selfsupervised/selfsupervised_mlp.py
+++ selfsupervised/selfsupervised_mlp.py
@@ -1,6 +1,3 @@
-
-# Your code here
-#!/usr/bin/env python3
 import os
 import sys
 import speechbrain as sb
@@ -9,14 +6,20 @@ from hyperpyyaml import load_hyperpyyaml
 
 class SelfSupervisedSpeakerCounter(sb.Brain):
     def compute_forward(self, batch, stage):
-        """Computation pipeline based on a encoder + emotion classifier.
+        """
+        Forward pass for generating predictions from input batches.
+
+        Parameters:
+        - batch (dict): The batch of data to process.
+        - stage (sb.Stage): The stage of the process (TRAIN, VALID, or TEST).
+
+        Returns:
+        - outputs (Tensor): The output predictions from the classifier.
         """
         batch = batch.to(self.device)
         wavs, lens = batch.sig
 
         outputs = self.modules.ssl_model(wavs, lens)
-
-        # last dim will be used for AdaptativeAVG pool
         outputs = self.hparams.avg_pool(outputs, lens)
         outputs = outputs.view(outputs.shape[0], -1)
 
@@ -25,8 +28,18 @@ class SelfSupervisedSpeakerCounter(sb.Brain):
         return outputs
 
     def compute_objectives(self, predictions, batch, stage):
-        """Computes the loss using speaker-id as label.
         """
+        Computes the loss for the current batch and stage.
+
+        Parameters:
+        - predictions (Tensor): The predictions made by the model.
+        - batch (dict): The batch of data including labels.
+        - stage (sb.Stage): The current stage (TRAIN, VALID, or TEST).
+
+        Returns:
+        - loss (Tensor): The computed loss value.
+        """
+
         spkid, _ = batch.num_speakers_encoded
 
         """to meet the input form of nll loss"""
@@ -38,52 +51,42 @@ class SelfSupervisedSpeakerCounter(sb.Brain):
         return loss
 
     def on_stage_start(self, stage, epoch=None):
-        """Gets called at the beginning of each epoch.
-        Arguments
-        ---------
-        stage : sb.Stage
-            One of sb.Stage.TRAIN, sb.Stage.VALID, or sb.Stage.TEST.
-        epoch : int
-            The currently-starting epoch. This is passed
-            `None` during the test stage.
+        """
+        Called at the beginning of each stage to setup metrics and state.
+
+        Parameters:
+        - stage (sb.Stage): The current stage (TRAIN, VALID, or TEST).
+        - epoch (int, optional): The current epoch number.
         """
 
-        # Set up statistics trackers for this stage
         self.loss_metric = sb.utils.metric_stats.MetricStats(
             metric=sb.nnet.losses.nll_loss
         )
 
-        # Set up evaluation-only statistics trackers
         if stage != sb.Stage.TRAIN:
             self.error_metrics = self.hparams.error_stats()
 
     def on_stage_end(self, stage, stage_loss, epoch=None):
-        """Gets called at the end of an epoch.
-        Arguments
-        ---------
-        stage : sb.Stage
-            One of sb.Stage.TRAIN, sb.Stage.VALID, sb.Stage.TEST
-        stage_loss : float
-            The average loss for all of the data processed in this stage.
-        epoch : int
-            The currently-starting epoch. This is passed
-            `None` during the test stage.
+        """
+        Called at the end of each stage to summarize and log the stage results.
+
+        Parameters:
+        - stage (sb.Stage): The current stage (TRAIN, VALID, or TEST).
+        - stage_loss (float): The average loss of the stage.
+        - epoch (int, optional): The current epoch number, if applicable.
         """
 
-        # Store the train loss until the validation stage.
         if stage == sb.Stage.TRAIN:
             self.train_loss = stage_loss
 
-        # Summarize the statistics from the stage for record-keeping.
         else:
             stats = {
                 "loss": stage_loss,
                 "error_rate": self.error_metrics.summarize("average"),
             }
 
-        # At the end of validation...
         if stage == sb.Stage.VALID:
-
+            # Learning rate adjustments and logging
             old_lr, new_lr = self.hparams.lr_annealing(stats["error_rate"])
             sb.nnet.schedulers.update_learning_rate(self.optimizer, new_lr)
 
@@ -95,19 +98,16 @@ class SelfSupervisedSpeakerCounter(sb.Brain):
                 self.ssl_optimizer, new_lr_ssl
             )
 
-            # The train_logger writes a summary to stdout and to the logfile.
             self.hparams.train_logger.log_stats(
                 {"Epoch": epoch, "lr": old_lr, "ssl_lr": old_lr_ssl},
                 train_stats={"loss": self.train_loss},
                 valid_stats=stats,
             )
 
-            # Save the current checkpoint and delete previous checkpoints,
             self.checkpointer.save_and_keep_only(
                 meta=stats, min_keys=["error_rate"]
             )
 
-        # We also write statistics about test data to stdout and to logfile.
         if stage == sb.Stage.TEST:
             self.hparams.train_logger.log_stats(
                 {"Epoch loaded": self.hparams.epoch_counter.current},
@@ -115,7 +115,9 @@ class SelfSupervisedSpeakerCounter(sb.Brain):
             )
 
     def init_optimizers(self):
-        "Initializes the ssl optimizer and model optimizer"
+        """
+        Initializes optimizers for the SSL model and the main model.
+        """
         self.ssl_optimizer = self.hparams.ssl_opt_class(
             self.modules.ssl_model.parameters()
         )
@@ -134,46 +136,48 @@ class SelfSupervisedSpeakerCounter(sb.Brain):
 
 
 def dataio_prep(hparams):
-    """This function prepares the datasets to be used in the brain class.
-    It also defines the data processing pipeline through user-defined
-    functions. We expect `prepare_mini_librispeech` to have been called before
-    this, so that the `train.json`, `valid.json`,  and `valid.json` manifest
-    files are available.
-    Arguments
-    ---------
-    hparams : dict
-        This dictionary is loaded from the `train.yaml` file, and it includes
-        all the hyperparameters needed for dataset construction and loading.
-    Returns
-    -------
-    datasets : dict
-        Contains two keys, "train" and "valid" that correspond
-        to the appropriate DynamicItemDataset object.
+    """
+    Prepares and returns datasets for training, validation, and testing.
+    Parameters:
+    - hparams (dict): A dictionary of hyperparameters for data preparation.
+    Returns:
+    - datasets (dict): A dictionary containing 'train', 'valid', and 'test' datasets.
     """
 
     # Define audio pipeline
     @sb.utils.data_pipeline.takes("wav_path")
     @sb.utils.data_pipeline.provides("sig")
     def audio_pipeline(wav_path):
-        """Load the signal, and pass it and its length to the corruption class.
-        This is done on the CPU in the `collate_fn`."""
+        """
+        Audio processing pipeline that loads and returns an audio signal.
+        Parameters:
+            - wav_path (str): Path to the audio file.
+        Returns:
+            - sig (Tensor): Loaded audio signal tensor.
+        """
+
         sig = sb.dataio.dataio.read_audio(wav_path)
         return sig
 
-    # Initialization of the label encoder. The label encoder assignes to each
-    # of the observed label a unique index (e.g, 'spk01': 0, 'spk02': 1, ..)
+    # Label Encoder
     label_encoder = sb.dataio.encoder.CategoricalEncoder()
 
     # Define label pipeline:
     @sb.utils.data_pipeline.takes("num_speakers")
     @sb.utils.data_pipeline.provides("num_speakers", "num_speakers_encoded")
     def label_pipeline(num_speakers):
+        """
+        Processes and encodes the number of speakers.
+        Parameters:
+             - num_speakers (int): The number of speakers in the audio.
+        Yields:
+            - num_speakers (int): The original number of speakers.
+            - num_speakers_encoded (Tensor): Encoded tensor of the number of speakers.
+        """
         yield num_speakers
         num_speakers_encoded = label_encoder.encode_label_torch(num_speakers)
         yield num_speakers_encoded
 
-    # Define datasets. We also connect the dataset with the data processing
-    # functions defined above.
     datasets = {}
     data_info = {
         "train": hparams["train_annotation"],
@@ -192,9 +196,6 @@ def dataio_prep(hparams):
             dynamic_items=[audio_pipeline, label_pipeline],
             output_keys=["id", "sig", "num_speakers_encoded"],
         )
-    # Load or compute the label encoder (with multi-GPU DDP support)
-    # Please, take a look into the lab_enc_file to see the label to index
-    # mappinng.
 
     lab_enc_file = os.path.join(hparams["save_folder"], "label_encoder.txt")
     label_encoder.load_or_create(
@@ -232,7 +233,7 @@ if __name__ == "__main__":
         hparams["ssl_model"].model.feature_extractor._freeze_parameters()
 
     # Initialize the Brain object to prepare for mask training.
-    num_speaker_brain = SelfSupervisedSpeakerCounter(
+    spkcounter = SelfSupervisedSpeakerCounter(
         modules=hparams["modules"],
         opt_class=hparams["opt_class"],
         hparams=hparams,
@@ -240,57 +241,57 @@ if __name__ == "__main__":
         checkpointer=hparams["checkpointer"],
     )
 
-    # The `fit()` method iterates the training loop, calling the methods
-    # necessary to update the parameters of the model. Since all objects
-    # with changing state are managed by the Checkpointer, training can be
-    # stopped at any point, and will be resumed on next call.
-    num_speaker_brain.fit(
-        epoch_counter=num_speaker_brain.hparams.epoch_counter,
+    spkcounter.fit(
+        epoch_counter=spkcounter.hparams.epoch_counter,
         train_set=datasets["train"],
         valid_set=datasets["valid"],
         train_loader_kwargs=hparams["dataloader_options"],
         valid_loader_kwargs=hparams["dataloader_options"],
     )
 
-    # # Load the best checkpoint for evaluation
-    # print("Evaluating on all classes")
-    # test_stats = num_speaker_brain.evaluate(
-    #     test_set=datasets["test"],
+    # Load the best checkpoint for evaluation
+    print("Evaluating on all classes")
+    test_stats = spkcounter.evaluate(
+        test_set=datasets["test"],
+        min_key="error_rate",
+        test_loader_kwargs=hparams["dataloader_options"],
+    )
+    
+    """
+    To get test accuracy on each class uncomment the code below.
+    """
+
+    # print("Evaluating on no spk class")
+    # test_stats = spkcounter.evaluate(
+    #     test_set=datasets["test_annotation_0_spk"],
     #     min_key="error_rate",
     #     test_loader_kwargs=hparams["dataloader_options"],
     # )
-
-    print("Evaluating on no spk class")
-    test_stats = num_speaker_brain.evaluate(
-        test_set=datasets["test_annotation_0_spk"],
-        min_key="error_rate",
-        test_loader_kwargs=hparams["dataloader_options"],
-    )
-
-    print("Evaluating on 1 spk class")
-    test_stats = num_speaker_brain.evaluate(
-        test_set=datasets["test_annotation_1_spk"],
-        min_key="error_rate",
-        test_loader_kwargs=hparams["dataloader_options"],
-    )
-
-    print("Evaluating on 2 spk class")
-    test_stats = num_speaker_brain.evaluate(
-        test_set=datasets["test_annotation_2_spk"],
-        min_key="error_rate",
-        test_loader_kwargs=hparams["dataloader_options"],
-    )
-
-    print("Evaluating on 3 spk class")
-    test_stats = num_speaker_brain.evaluate(
-        test_set=datasets["test_annotation_3_spk"],
-        min_key="error_rate",
-        test_loader_kwargs=hparams["dataloader_options"],
-    )
-
-    print("Evaluating on 4 spk class")
-    test_stats = num_speaker_brain.evaluate(
-        test_set=datasets["test_annotation_4_spk"],
-        min_key="error_rate",
-        test_loader_kwargs=hparams["dataloader_options"],
-    )
+    # 
+    # print("Evaluating on 1 spk class")
+    # test_stats = spkcounter.evaluate(
+    #     test_set=datasets["test_annotation_1_spk"],
+    #     min_key="error_rate",
+    #     test_loader_kwargs=hparams["dataloader_options"],
+    # )
+    # 
+    # print("Evaluating on 2 spk class")
+    # test_stats = spkcounter.evaluate(
+    #     test_set=datasets["test_annotation_2_spk"],
+    #     min_key="error_rate",
+    #     test_loader_kwargs=hparams["dataloader_options"],
+    # )
+    # 
+    # print("Evaluating on 3 spk class")
+    # test_stats = spkcounter.evaluate(
+    #     test_set=datasets["test_annotation_3_spk"],
+    #     min_key="error_rate",
+    #     test_loader_kwargs=hparams["dataloader_options"],
+    # )
+    # 
+    # print("Evaluating on 4 spk class")
+    # test_stats = spkcounter.evaluate(
+    #     test_set=datasets["test_annotation_4_spk"],
+    #     min_key="error_rate",
+    #     test_loader_kwargs=hparams["dataloader_options"],
+    # )
